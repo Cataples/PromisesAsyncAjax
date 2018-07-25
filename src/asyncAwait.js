3f7b89330580eff655a722fcef4fc1ddfef6d719
Ples Catalin Andrei <catalin.ples@fortech.ro>
2018-07-25 09:53:20 +0300

    added async await

--- src/asyncAwait.js
+++ src/asyncAwait.js
@@ -1,5 +1,5 @@
 const asTestFunc = async url => {
-    const response = await fetch ( url);
+    const response = await fetch ( url );
     const joke = await response.json();
     const jokeData = await joke.value;
     if ( response.status !== 200 ){
@@ -23,5 +23,29 @@ const asTestFunc = async url => {
 // })();
 
 
+
+//  cu async await
+// var fruits = true;
+
+// const getFruits = new Promise( ( resolve, reject ) => {
+//     if ( fruits ) {
+//         resolve({
+//             banane: 10,
+//             mere: 2,
+//             struguri: 5
+//         })
+//     } else {
+//         reject( "Nu e nevoie de fructe" );
+//     }
+// }) 
+
+// async function asyncCall(){
+//     const r1 = await getFruits;
+//     const r2 = await r1;
+//     console.log(r2)
+//   }
+  
+//   asyncCall();
+  
 
 export { asTestFunc };
